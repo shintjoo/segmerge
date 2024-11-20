ae5a30447fd9d1843257c0e48f616fc63fd7befd
Shawn Zhu <shawnkzhu@gmail.com>
2024-11-19 19:25:13 -0600

    Update main.cu

--- main.cu
+++ main.cu
@@ -12,25 +12,25 @@
             << std::endl; \
   return 0; }
 
-
 int main (int argc, char* argv[]) {
 
   using K = int;
   using T = int;
 
-  if (argc != 2) {
-    std::cerr << "usage: ./run n\n";
+  if (argc != 3) {
+    std::cerr << "usage: ./run n_a n_b\n";
     std::exit(1);
   }
 
   // params
-  int n = std::atoi(argv[1]); // size of array
+  int n_a = std::atoi(argv[1]); // size of array A
+  int n_b = std::atoi(argv[2]); // size of array B
   int largest_key = 20;
   int max_seg_size = 10;
   int min_seg_size = 0;
   
   // info
-  std::cout << "Merging two arrays of " << n << " keys and vals\n";
+  std::cout << "Merging two arrays of " << n_a << " and " << n_b << " keys and vals\n";
   std::cout << "Largest key       : " << largest_key << std::endl;
   std::cout << "Smallest key      : " << 0 << std::endl;
   std::cout << "Largest value     : " << 1 << std::endl;
@@ -38,41 +38,48 @@ int main (int argc, char* argv[]) {
   std::cout << "Largest seg size  : " << max_seg_size << std::endl;
   std::cout << "Smallest seg size : " << min_seg_size << std::endl; 
 
-
   // seed
   srand(static_cast<unsigned>(time(0)));
 
-  // create arrays
-  std::vector<K> key_a(n);
-  std::vector<K> key_b(n);
+  // create arrays for key_a, key_b, val_a, val_b
+  std::vector<K> key_a(n_a);
+  std::vector<K> key_b(n_b);
   std::vector<K> key_c;
-  std::vector<T> val_a(n);
-  std::vector<T> val_b(n);
+  std::vector<T> val_a(n_a);
+  std::vector<T> val_b(n_b);
   std::vector<T> val_c;
-  
-  for (std::size_t i = 0; i < n; i++) {
+
+  // populate arrays
+  for (std::size_t i = 0; i < n_a; i++) {
     key_a[i] = std::rand() % largest_key;
+    val_a[i] = 1; // can be randomized as well
+  }
+  for (std::size_t i = 0; i < n_b; i++) {
     key_b[i] = std::rand() % largest_key;
-  }
-  for (std::size_t i = 0; i < n; i++) {
-    val_a[i] = 1;
-    val_b[i] = 1;
+    val_b[i] = 1; // can be randomized as well
   }
 
-  std::vector<int> seg;
-  std::vector<int> seg_c;
-  int start = 0;
-  while (start < n) {
-    seg.emplace_back(start);
-    //int sz = std::rand() % (max_seg_size - min_seg_size + 1) + min_seg_size;
+  // Segmentations
+  std::vector<int> seg_a, seg_b, seg_c;
+  int m_a = seg_a.size();
+  int m_b = seg_b.size();
+  int start_a = 0, start_b = 0;
+  
+  while (start_a < n_a) {
+    seg_a.emplace_back(start_a);
     int sz = std::rand() % max_seg_size + min_seg_size;
-    start = seg.back() + sz;
+    start_a = seg_a.back() + sz;
   }
-  int m = seg.size();
-  seg_c.emplace_back(0);
-  std::cout << "Number of segments: " << m+1 << std::endl;
 
-  // allocate gpu memory
+  while (start_b < n_b) {
+    seg_b.emplace_back(start_b);
+    int sz = std::rand() % max_seg_size + min_seg_size;
+    start_b = seg_b.back() + sz;
+  }
+  
+  seg_c.emplace_back(0);
+  
+  // allocate GPU memory
   cudaError_t err;
   K* key_a_d;
   K* key_b_d;
@@ -80,42 +87,49 @@ int main (int argc, char* argv[]) {
   T* val_a_d;
   T* val_b_d;
   T* val_c_d;
-  int* seg_d;
+  int* seg_a_d;
+  int* seg_b_d;
   int* seg_c_d;
-  err = cudaMalloc(&key_a_d, sizeof(K)*n);
+
+  err = cudaMalloc(&key_a_d, sizeof(K)*n_a);
   CUDA_CHECK(err, "alloc key_a_d");
-  err = cudaMalloc(&key_b_d, sizeof(K)*n);
+  err = cudaMalloc(&key_b_d, sizeof(K)*n_b);
   CUDA_CHECK(err, "alloc key_b_d");
-  err = cudaMalloc(&key_c_d, sizeof(K)*2*n);
+  err = cudaMalloc(&key_c_d, sizeof(K)*(n_a + n_b)); // At most size of A + B
   CUDA_CHECK(err, "alloc key_c_d");
-  err = cudaMalloc(&val_a_d, sizeof(T)*n);
+  err = cudaMalloc(&val_a_d, sizeof(T)*n_a);
   CUDA_CHECK(err, "alloc val_a_d");
-  err = cudaMalloc(&val_b_d, sizeof(T)*n);
+  err = cudaMalloc(&val_b_d, sizeof(T)*n_b);
   CUDA_CHECK(err, "alloc val_b_d");
-  err = cudaMalloc(&val_c_d, sizeof(T)*2*n);
+  err = cudaMalloc(&val_c_d, sizeof(T)*(n_a + n_b));
   CUDA_CHECK(err, "alloc val_c_d");
-  err = cudaMalloc(&seg_d, sizeof(int)*m);
-  CUDA_CHECK(err, "alloc seg_d");
-  err = cudaMalloc(&seg_c_d, sizeof(int)*m);
-  CUDA_CHECK(err, "alloc seg_d_d");
-    
-  // copy h2d
-  err = cudaMemcpy(key_a_d, &key_a[0], sizeof(K)*n, cudaMemcpyDefault);
+  err = cudaMalloc(&seg_a_d, sizeof(int)*seg_a.size());
+  CUDA_CHECK(err, "alloc seg_a_d");
+  err = cudaMalloc(&seg_b_d, sizeof(int)*seg_b.size());
+  CUDA_CHECK(err, "alloc seg_b_d");
+  err = cudaMalloc(&seg_c_d, sizeof(int)*seg_c.size());
+  CUDA_CHECK(err, "alloc seg_c_d");
+
+  // copy data from host to device
+  err = cudaMemcpy(key_a_d, &key_a[0], sizeof(K)*n_a, cudaMemcpyHostToDevice);
   CUDA_CHECK(err, "copy to key_a_d"); 
-  err = cudaMemcpy(key_b_d, &key_b[0], sizeof(K)*n, cudaMemcpyDefault);
+  err = cudaMemcpy(key_b_d, &key_b[0], sizeof(K)*n_b, cudaMemcpyHostToDevice);
   CUDA_CHECK(err, "copy to key_b_d"); 
-  err = cudaMemcpy(val_a_d, &val_a[0], sizeof(T)*n, cudaMemcpyDefault);
+  err = cudaMemcpy(val_a_d, &val_a[0], sizeof(T)*n_a, cudaMemcpyHostToDevice);
   CUDA_CHECK(err, "copy to val_a_d"); 
-  err = cudaMemcpy(val_b_d, &val_b[0], sizeof(T)*n, cudaMemcpyDefault);
+  err = cudaMemcpy(val_b_d, &val_b[0], sizeof(T)*n_b, cudaMemcpyHostToDevice);
   CUDA_CHECK(err, "copy to val_b_d"); 
-  err = cudaMemcpy(seg_d, &seg[0], sizeof(int)*m, cudaMemcpyDefault);
-  CUDA_CHECK(err, "copy to seg_d"); 
+  err = cudaMemcpy(seg_a_d, &seg_a[0], sizeof(int)*seg_a.size(), cudaMemcpyHostToDevice);
+  CUDA_CHECK(err, "copy to seg_a_d"); 
+  err = cudaMemcpy(seg_b_d, &seg_b[0], sizeof(int)*seg_b.size(), cudaMemcpyHostToDevice);
+  CUDA_CHECK(err, "copy to seg_b_d");
 
   auto begin = std::chrono::steady_clock::now();
   int new_n = segmerge(
     key_a_d, key_b_d, key_c_d,
     val_a_d, val_b_d, val_c_d,
-    seg_d,   seg_c_d, n, m
+    seg_a_d, seg_b_d, seg_c_d, 
+    n_a, n_b, m_a, m_b
   );
   cudaDeviceSynchronize();
   auto end = std::chrono::steady_clock::now();
@@ -123,18 +137,18 @@ int main (int argc, char* argv[]) {
     std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()
     << std::endl;
 
-  // copy d2h
-  std::vector<int> seg_c_h(m);
+  // copy data from device to host
+  std::vector<int> seg_c_h(seg_c.size());
   std::vector<K> key_c_h(new_n); 
   std::vector<T> val_c_h(new_n); 
-  err = cudaMemcpy(&seg_c_h[0], seg_c_d, sizeof(int)*m, cudaMemcpyDefault);
+  err = cudaMemcpy(&seg_c_h[0], seg_c_d, sizeof(int)*seg_c.size(), cudaMemcpyDeviceToHost);
   CUDA_CHECK(err, "copy from seg_c_d");
-  err = cudaMemcpy(&key_c_h[0], key_c_d, sizeof(K)*new_n, cudaMemcpyDefault); 
+  err = cudaMemcpy(&key_c_h[0], key_c_d, sizeof(K)*new_n, cudaMemcpyDeviceToHost); 
   CUDA_CHECK(err, "copy from key_c_d");
-  err = cudaMemcpy(&val_c_h[0], val_c_d, sizeof(T)*new_n, cudaMemcpyDefault);
+  err = cudaMemcpy(&val_c_h[0], val_c_d, sizeof(T)*new_n, cudaMemcpyDeviceToHost);
   CUDA_CHECK(err, "copy from val_c_d");
 
-  // free
+  // free GPU memory
   err = cudaFree(key_a_d);
   CUDA_CHECK(err, "free key_a_d");
   err = cudaFree(key_b_d);
@@ -147,43 +161,61 @@ int main (int argc, char* argv[]) {
   CUDA_CHECK(err, "free val_b_d"); 
   err = cudaFree(val_c_d);
   CUDA_CHECK(err, "free val_c_d");
-  err = cudaFree(seg_d);
-  CUDA_CHECK(err, "free seg_d");
+  err = cudaFree(seg_a_d);
+  CUDA_CHECK(err, "free seg_a_d");
+  err = cudaFree(seg_b_d);
+  CUDA_CHECK(err, "free seg_b_d");
+  err = cudaFree(seg_c_d);
+  CUDA_CHECK(err, "free seg_c_d");
 
-  // cpu
+  // CPU-based validation
   begin = std::chrono::steady_clock::now();
-  gold_segsort(key_a, val_a, n, seg, m);
-  gold_segsort(key_b, val_b, n, seg, m);
+  gold_segsort(key_a, val_a, n_a, seg_a, m_a);
+  gold_segsort(key_b, val_b, n_b, seg_b, m_b);
   gold_segmerge(key_a, key_b, key_c,
                 val_a, val_b, val_c,
-                n, seg, m, seg_c);
+                n_a, n_b, seg_a, seg_b, seg_c);
   end = std::chrono::steady_clock::now();
-  std::cout << "CPU runtime (us)  : " << 
+  std::cout << "CPU runtime (us) : " <<
     std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()
     << std::endl;
-  
-  
+
+  return 0;
+
   // check
   int cnt = 0;
-  for (std::size_t i = 0; i < m; i++)
+
+  // Compare segmentations
+  for (std::size_t i = 0; i < seg_c.size(); i++) {
     if (seg_c[i] != seg_c_h[i]) cnt++;
-  if (cnt != 0) 
-    std::cout << "[NOT PASSED] checking segs: #err = " <<  cnt << std::endl;
-  else 
+  }
+  if (cnt != 0) {
+    std::cout << "[NOT PASSED] checking segs: #err = " << cnt << std::endl;
+  } else {
     std::cout << "[PASSED] checking segs\n";
+  }
+
+  // Reset count for keys comparison
   cnt = 0;
-  for (std::size_t i = 0; i < new_n; i++)
+  for (std::size_t i = 0; i < new_n; i++) {
     if (key_c[i] != key_c_h[i]) cnt++;
-  if (cnt != 0)  
-    std::cout << "[NOT PASSED] checking keys: #err = " <<  cnt << std::endl;
-  else
+  }
+  if (cnt != 0) {
+    std::cout << "[NOT PASSED] checking keys: #err = " << cnt << std::endl;
+  } else {
     std::cout << "[PASSED] checking keys\n";
-  for (std::size_t i = 0; i < new_n; i++)
+  }
+
+  // Reset count for values comparison
+  cnt = 0;
+  for (std::size_t i = 0; i < new_n; i++) {
     if (val_c[i] != val_c_h[i]) cnt++;
-  if (cnt != 0)  
-    std::cout << "[NOT PASSED] checking vals: #err = " <<  cnt << std::endl;
-  else
+  }
+  if (cnt != 0) {
+    std::cout << "[NOT PASSED] checking vals: #err = " << cnt << std::endl;
+  } else {
     std::cout << "[PASSED] checking vals\n";
+  }
 
   // print
   //std::cout << "key_c:\n"; 
