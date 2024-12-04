50565cdcb3964e369fe992a7b4bfe9b01b81916f
Shawn Zhu <shawnkzhu@gmail.com>
2024-12-03 21:45:33 -0600

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+#SBATCH -J final759
+#SBATCH -p instruction
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH -t 0-00:05:00
+#SBATCH -o final759.out -e final759.err
+
+module load nvidia/cuda/11.8.0
+module load gcc/11.3.0
+
+mkdir build
+cd build
+cmake ..
+make
+make test
+./run 128
